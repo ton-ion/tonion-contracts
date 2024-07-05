6cd033bb3a6494a16c82497446b978899fae8031
ZigBalthazar <zigbalthazar@gmail.com>
2024-07-05 23:52:16 +0330

    test: add more test for paymentSplitter

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,5 +1,6 @@
 import { shouldBehaveLikeAccessControl } from './access/AccessControl.behavior';
 import { shouldBehaveLikeOwnableTransferable2Step } from './access/OwnableTransferable2Step';
+import { shouldBehaveLikePaymentSplitter } from './payments/paymentSplitter.behavior';
 import { shouldBehaveLikeBasicJetton } from './tokens/jetton/basic/Basic.behavior';
 import { shouldBehaveLikeApproveableJetton } from './tokens/jetton/extensions/approveable.behavior';
 import { shouldBehaveLikeMaxSupplyJetton } from './tokens/jetton/extensions/maxsupply.behavior';
@@ -34,6 +35,12 @@ describe('OpenGem-contract UnitTests', function () {
                     // shouldBehaveLikeApproveableJetton();
                 });
             });
+        });
+    });
+
+    describe('Payments', function () {
+        describe('paymentSplitter', function () {
+            shouldBehaveLikePaymentSplitter();
         });
     });
 });
