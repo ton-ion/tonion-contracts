9ee4c18520a249d0cd6f811f9834ddde96f9e537
Zig Blathazar <42387185+ZigBalthazar@users.noreply.github.com>
2024-07-08 19:54:10 +0330

    Revert "feat(utils): string utility module (#13)"
    This reverts commit 5c0e9a6f5914e38c5ee593b012aff820c83a69cc.

--- wrappers/stringutil.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'tact',
-    target: 'contracts\traits\string.tact', 
-    options: {
-        debug: true,
-    },
-};
